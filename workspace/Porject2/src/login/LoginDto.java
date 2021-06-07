64cbe5643a88795f1a116488214de3068306e3e4
kaaang <kaaang@kakao.com>
2021-06-07 23:36:49 +0900

    로그인DTO정보 추가

--- workspace/Porject2/src/login/LoginDto.java
+++ workspace/Porject2/src/login/LoginDto.java
@@ -1,16 +1,19 @@
 package login;
 
 public class LoginDto {
+	private int pk_user;
 	private String id;
 	private String pass;
+	private String name;
+	private String phone;
+	private String email;
+	private int level;
 	private String user_nickname;
-	
-	
-	public String getUser_nickname() {
-		return user_nickname;
+	public int getPk_user() {
+		return pk_user;
 	}
-	public void setUser_nickname(String user_nickname) {
-		this.user_nickname = user_nickname;
+	public void setPk_user(int pk_user) {
+		this.pk_user = pk_user;
 	}
 	public String getId() {
 		return id;
@@ -24,6 +27,39 @@ public class LoginDto {
 	public void setPass(String pass) {
 		this.pass = pass;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public String getUser_nickname() {
+		return user_nickname;
+	}
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
+	}
+	
+	
+	
 
 	
 	
